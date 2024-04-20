5aa3bd3e971ed65540b7fc04766406d99258fd71
Geeko24 <kingnorway17@gmail.com>
2024-04-21 00:45:12 +0800

    update

--- app/(root)/(dashboard)/questions/[id]/page.jsx
+++ app/(root)/(dashboard)/questions/[id]/page.jsx
@@ -1,8 +1,11 @@
 import QuestionDetails from '@/components/QuestionDetails'
 import React from 'react'
 import { getQuestionById } from "@/lib/actions/question.action";
+import { revalidatePath } from 'next/cache';
 const Question =  async ({ params }) => {
   const result = await getQuestionById(params.id);
+  revalidatePath('/')
+
   return (
     <QuestionDetails result={result} />
   )
