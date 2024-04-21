3e81fff65b33d34ee06b0ebd6f4404c3b813fb7a
Geeko24 <kingnorway17@gmail.com>
2024-04-21 19:19:44 +0800

    update

--- components/UsersQuestion.jsx
+++ components/UsersQuestion.jsx
@@ -4,7 +4,7 @@ const UsersQuestion = async ({ questions }) => {
 	return (
 		<>
 			{questions.length === 0 ? (
-				<h2 className="mt-10 text-2xl">You don't have any question</h2>
+				<h2 className="mt-10 text-2xl">You don&apos;t have any question</h2>
 			) : (
 				<>
 					<h2 className="mt-10 text-2xl">
